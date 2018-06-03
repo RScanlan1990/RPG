59998199b301f735f50778650e63f3bc1732235b
Rory Sky Scanlan <roryscanlan@live.co.uk>
2018-06-03 22:55:34 +0100

    bug fix, player rotation - merge to master

--- Assets/RS/Player/Scripts/Movement.cs
+++ Assets/RS/Player/Scripts/Movement.cs
@@ -22,13 +22,12 @@ public class Movement : MonoBehaviour
 
     public void MoveTowardsWorldPositon(Clickable.ClickReturn clickReturn, Vector3 clickPosition)
     {
+        var lookPos = clickPosition - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 100.0F);
         if (clickReturn == null)
         {
-            var lookPos = clickPosition - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 100.0F);
-
             clickPosition.y = transform.position.y;
             this.transform.position += (clickPosition - this.transform.position).normalized * PlayerSpeed * Time.fixedDeltaTime * Mathf.Clamp(Vector3.Distance(clickPosition, transform.position), 0.0f, 1.0f);
         }
